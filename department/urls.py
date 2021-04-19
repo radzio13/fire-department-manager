80aee2b5caac7eb98b4a4659a51c32a05f94e861
damian0s10 <damian.wasiluk10@gmail.com>
2021-04-19 19:57:01 +0200

    vehicle page

--- department/urls.py
+++ department/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('sprzet/', views.EquipmentView.as_view(), name='equipment')
+    path('sprzet/', views.EquipmentView.as_view(), name='equipment'),
+    path('pojazdy/', views.VehicleView.as_view(), name='vehicle')
 ]
--- department/views.py
+++ department/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
-
+from django.views.generic.base import TemplateResponseMixin, View
+from .models import *
 
 class HomeView(TemplateView):
     template_name = 'department/home.html'
@@ -7,3 +8,10 @@ class HomeView(TemplateView):
 
 class EquipmentView(TemplateView):
     template_name = 'department/equipment.html'
+    
+class VehicleView(TemplateResponseMixin, View):
+    template_name = 'department/vehicle.html'
+
+    def get(self, request):
+        vehicles = Pojazdy.objects.all()
+        return self.render_to_response({"vehicles": vehicles})

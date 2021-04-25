dd880e57bcb25e3b55222f92e412aa126703d7ff
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2021-04-25 17:58:16 +0200

    Set default pk type to AutoField

--- project/settings.py
+++ project/settings.py
@@ -141,3 +141,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default pk type
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
